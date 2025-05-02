de30ce7d15a9249da603cf9c25b69039716b0bfd
BetterLuckNextLife <rykovegor020@gmail.com>
2025-05-02 11:22:31 +0300

    Added scanHost function

--- main.go
+++ main.go
@@ -8,28 +8,25 @@ import (
 	"time"
 )
 
-func pingPort(host, port, protocol string, found chan int, wg *sync.WaitGroup) {
+// Scans a ports and if it is open writes it to a chennel
+func scanPort(host, port, protocol string, found chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	_, err := net.DialTimeout(protocol, host+":"+port, 1000*time.Millisecond)
+	_, err := net.DialTimeout(protocol, host+":"+port, 2500*time.Millisecond)
 	if err != nil {
 	} else {
 		foundport, _ := strconv.Atoi(port)
 		found <- foundport
 	}
-
 }
 
-func main() {
-	ip := "127.0.0.1"
-	//port := "22"
-	protocol := "tcp"
-
+// Scans all the ports on a host, prints found ports
+func scanHost(host, protocol string) {
 	found := make(chan int, 65535)
 	var wg sync.WaitGroup
 
-	for i := 0; i <= 65535; i++ {
+	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
-		go pingPort(ip, strconv.Itoa(i), protocol, found, &wg)
+		go scanPort(host, strconv.Itoa(i), protocol, found, &wg)
 	}
 
 	wg.Wait()
@@ -38,4 +35,10 @@ func main() {
 	for port := range found {
 		fmt.Println(port)
 	}
+}
+
+func main() {
+	ip := "192.168.100.2"
+	protocol := "tcp"
+	scanHost(ip, protocol)
 }
